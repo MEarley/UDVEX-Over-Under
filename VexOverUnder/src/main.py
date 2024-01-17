c370124a02cb973cea893752ee98f4ed53a02a44
MEarley <113404793+MEarley@users.noreply.github.com>
2024-01-17 18:36:29 -0500

    Extra mode & catapult motors
    -revamped mode selection (3 total)
    -added catapult controls
    -removed motor 4 from group

--- VexOverUnder/src/main.py
+++ VexOverUnder/src/main.py
@@ -13,6 +13,7 @@
 # Library imports
 from vex import *
 import math
+from enum import Enum
 
 # Constants
 WHEELSIZE = 2.75    # Inches Diameter
@@ -21,6 +22,13 @@ TILEREVOLUTIONS = TILEDISTANCE / (math.pi * WHEELSIZE)  # Revolutions per Tile (
 EXPONENTIALCONSTANT = 21.71472409516259138255644594583
 ROTATIONALOFFSET = 7.5
 KP = 0.01
+
+class mode(Enum):
+    TANK = 1
+    ARCADE = 2
+    ARCADE_SPEED = 3
+    WINTER = 4
+
 
 # Definitions 
 # The Brain
@@ -54,19 +62,22 @@ BLUE_MOTOR = GearSetting.RATIO_6_1
 left_motor_1 = Motor(Ports.PORT2, BLUE_MOTOR, True)
 left_motor_2 = Motor(Ports.PORT4, BLUE_MOTOR, True)
 left_motor_3 = Motor(Ports.PORT6, BLUE_MOTOR, True)
-left_motor_4 = Motor(Ports.PORT8, BLUE_MOTOR, True)
-left_motor_group = MotorGroup(left_motor_1,left_motor_2,left_motor_3,left_motor_4)
+#left_motor_4 = Motor(Ports.PORT8, BLUE_MOTOR, True)
+left_motor_group = MotorGroup(left_motor_1,left_motor_2,left_motor_3)
 
 #Right Motors
 right_motor_1 = Motor(Ports.PORT3, BLUE_MOTOR, False)
 right_motor_2 = Motor(Ports.PORT5, BLUE_MOTOR, False)
 right_motor_3 = Motor(Ports.PORT7, BLUE_MOTOR, False)
-right_motor_4 = Motor(Ports.PORT9, BLUE_MOTOR, False)
-right_motor_group = MotorGroup(right_motor_1,right_motor_2,right_motor_3,right_motor_4)
+#right_motor_4 = Motor(Ports.PORT9, BLUE_MOTOR, False)
+right_motor_group = MotorGroup(right_motor_1,right_motor_2,right_motor_3)
 
 # Catapult Motor
-catapult_motor = Motor(Ports.PORT1,BLUE_MOTOR,False)
-catapult_motor.set_max_torque(100,PERCENT)
+catapult_motor_FW = Motor(Ports.PORT8,BLUE_MOTOR,False)
+catapult_motor_BW = Motor(Ports.PORT9,BLUE_MOTOR,True)
+catapult_motor_FW.set_max_torque(100,PERCENT)
+catapult_motor_BW.set_max_torque(100,PERCENT)
+catapult_motor = MotorGroup(catapult_motor_FW,catapult_motor_BW)
 
 # Intake motor
 intake_motor = Motor(Ports.PORT10, GREEN_MOTOR, True)
@@ -153,14 +164,14 @@ def left_spin_volt(direction, voltage):
     left_motor_1.spin(direction,voltage,VOLT)
     left_motor_2.spin(direction,voltage,VOLT)
     left_motor_3.spin(direction,voltage,VOLT)
-    left_motor_4.spin(direction,voltage,VOLT)
+    #left_motor_4.spin(direction,voltage,VOLT)
     return
 
 def right_spin_volt(direction,voltage):
     right_motor_1.spin(direction,voltage,VOLT)
     right_motor_2.spin(direction,voltage,VOLT)
     right_motor_3.spin(direction,voltage,VOLT)
-    right_motor_4.spin(direction,voltage,VOLT)
+    #right_motor_4.spin(direction,voltage,VOLT)
     return
 
 def pre_autonomous():
@@ -282,107 +293,166 @@ def autonomous():
     #right_motor_group.spin_to_position(rotation=180,units=RotationUnits.DEG,velocity=50,wait=True)
 
 
+def tank_drive():
+    brain.screen.clear_screen(Color.GREEN)
+    brain.screen.print("Tank-Drive Control")
+
+    # Controlelr joystick positions
+    leftAxis_UpDwn = controller.axis3.position()
+    rightAxis_UpDwn = controller.axis2.position()
+
+    # Determines whether or not the robot should be going forward to backward
+    isNegative_Left = False if leftAxis_UpDwn >= 0 else True
+    isNegative_Right = False if rightAxis_UpDwn >= 0 else True
+
+    # Calculate left velocity percentage based on logarithmic scale (0 < leftAxis_UpDwn <= 100)
+    #left_speed = (int)(50.0 * (math.log(abs(leftAxis_UpDwn)+1,10)))
+
+    left_speed = (int)(math.exp(abs(leftAxis_UpDwn) / EXPONENTIALCONSTANT))
+
+    if(left_speed > 5):
+        left_motor_group.spin(FORWARD)
+        if(isNegative_Left):
+            left_speed *= -1
+        #left_motor_group.set_velocity(left_speed,units=VoltageUnits)
+
+        # Set the velocity depending on the axis position
+        left_motor_group.set_velocity(left_speed,PERCENT)
+    else:
+        left_motor_group.stop(COAST)
+
+    #right_speed = controller.axis2.position()
+
+    # Calculate left velocity percentage based on logarithmic scale (0 < rightAxis_UpDwn <= 100)
+    #right_speed = (int)(50.0 * (math.log(abs(rightAxis_UpDwn)+1,10)))
+
+    right_speed = (int)(math.exp(abs(rightAxis_UpDwn) / EXPONENTIALCONSTANT))
+
+    if(right_speed > 5):
+        right_motor_group.spin(FORWARD)
+        if(isNegative_Right):
+            right_speed *= -1
+
+        # Set the velocity depending on the axis position
+        right_motor_group.set_velocity(right_speed,PERCENT)
+    else:
+        right_motor_group.stop(COAST)    
+
+    return
+
+def arcade_drive():
+    brain.screen.clear_screen(Color.BLUE)
+    brain.screen.print("Arcade-Drive Control")
+
+    # Controls for Up-Down and Left-Right movement
+    leftAxis_UpDwn = controller.axis3.position()
+    leftAxis_LR = controller.axis4.position()
+
+    # Determines whether or not the robot should be going forward or backward, left or right
+    isNegativeY = False if leftAxis_UpDwn >= 0 else True
+    isNegativeX = False if leftAxis_LR >= 0 else True
+
+    # Calculate exponentional based speed
+    ySpeed = (int)(math.exp(abs(leftAxis_UpDwn) / EXPONENTIALCONSTANT))
+    xSpeed = (int)(math.exp(abs(leftAxis_LR) / EXPONENTIALCONSTANT))
+
+    if(ySpeed > 5 or xSpeed > 5):
+        left_motor_group.spin(FORWARD)
+        right_motor_group.spin(FORWARD)
+
+        if(isNegativeY):
+            ySpeed *= -1
+        if(isNegativeX):
+            xSpeed *= -1
+
+        # Set the velocity depending on the axis position
+        left_motor_group.set_velocity(ySpeed + xSpeed,PERCENT)
+        right_motor_group.set_velocity(ySpeed - xSpeed,PERCENT)
+    else:
+        left_motor_group.stop(COAST)
+        right_motor_group.stop(COAST)
+
+    return
+
+def arcade_speed_drive():
+    brain.screen.clear_screen(Color.PURPLE)
+    brain.screen.print("Arcade-Drive-Speed Control")
+
+    # Controls for Up-Down and Left-Right movement
+    leftAxis_UpDwn = controller.axis3.position()
+    leftAxis_LR = controller.axis4.position()
+
+    # Determines whether or not the robot should be going forward or backward, left or right
+    isNegativeY = False if leftAxis_UpDwn >= 0 else True
+    isNegativeX = False if leftAxis_LR >= 0 else True
+
+    # Calculate exponentional based speed
+    ySpeed = abs(leftAxis_UpDwn) 
+    xSpeed = abs(leftAxis_LR)
+
+    if(ySpeed > 5 or xSpeed > 5):
+        left_motor_group.spin(FORWARD)
+        right_motor_group.spin(FORWARD)
+
+        if(isNegativeY):
+            ySpeed *= -1
+        if(isNegativeX):
+            xSpeed *= -1
+
+        # Set the velocity depending on the axis position
+        left_motor_group.set_velocity(ySpeed + xSpeed,PERCENT)
+        right_motor_group.set_velocity(ySpeed - xSpeed,PERCENT)
+    else:
+        left_motor_group.stop(COAST)
+        right_motor_group.stop(COAST)
+
+    return
+
 def user_control():
-    Control_Mode = True 
+    Control_Mode = mode.TANK
     print(TILEREVOLUTIONS)
     print(TILEDISTANCE)
     while(True):
         # Switch between control modes
-        if(controller.buttonUp.pressing()):
-            Control_Mode = not Control_Mode
-            wait(0.5,SECONDS)
+        if(controller.buttonUp.pressing()): # D-pad Up
+            Control_Mode = mode.TANK
+        elif(controller.buttonRight.pressing()): # D-pad Right
+            Control_Mode = mode.ARCADE
+        elif(controller.buttonDown.pressing()): # D-pad Down
+            Control_Mode = mode.ARCADE_SPEED
+        elif(controller.buttonLeft.pressing()): # D-pad Left
+            Control_Mode = mode.TANK # Extra mode slot
+        wait(0.5,SECONDS)
 
-        # Control Modes: 1 = Manual Tank Drive, 2 = Joystick control
-        if(Control_Mode):
-            brain.screen.clear_screen(Color.GREEN)
-            brain.screen.print("Tank-Drive Control")
-            R1 = bool(controller.buttonR1.pressing())
-            R2 = bool(controller.buttonR2.pressing())
+        R1 = bool(controller.buttonR1.pressing())
+        R2 = bool(controller.buttonR2.pressing())
 
-
-            # Intake control
-            if(R1 == True and R2 == False):
-                print("Forward")
-                intake_motor.spin(REVERSE)
-            elif ((R2 == True) and (R1 == False)):
-                print("Reverse")
-                intake_motor.spin(FORWARD)
-            else:
-                intake_motor.stop()
-            #left_speed = controller.axis3.position()
-
-            # Controlelr joystick positions
-            leftAxis_UpDwn = controller.axis3.position()
-            rightAxis_UpDwn = controller.axis2.position()
-
-            # Determines whether or not the robot should be going forward to backward
-            isNegative_Left = False if leftAxis_UpDwn >= 0 else True
-            isNegative_Right = False if rightAxis_UpDwn >= 0 else True
-
-            # Calculate left velocity percentage based on logarithmic scale (0 < leftAxis_UpDwn <= 100)
-            #left_speed = (int)(50.0 * (math.log(abs(leftAxis_UpDwn)+1,10)))
-
-            left_speed = (int)(math.exp(abs(leftAxis_UpDwn) / EXPONENTIALCONSTANT))
-
-            if(left_speed > 5):
-                left_motor_group.spin(FORWARD)
-                if(isNegative_Left):
-                    left_speed *= -1
-                #left_motor_group.set_velocity(left_speed,units=VoltageUnits)
-
-                # Set the velocity depending on the axis position
-                left_motor_group.set_velocity(left_speed,PERCENT)
-            else:
-                left_motor_group.stop(COAST)
-
-            #right_speed = controller.axis2.position()
-
-            # Calculate left velocity percentage based on logarithmic scale (0 < rightAxis_UpDwn <= 100)
-            #right_speed = (int)(50.0 * (math.log(abs(rightAxis_UpDwn)+1,10)))
-
-            right_speed = (int)(math.exp(abs(rightAxis_UpDwn) / EXPONENTIALCONSTANT))
-
-            if(right_speed > 5):
-                right_motor_group.spin(FORWARD)
-                if(isNegative_Right):
-                    right_speed *= -1
-
-                # Set the velocity depending on the axis position
-                right_motor_group.set_velocity(right_speed,PERCENT)
-            else:
-                right_motor_group.stop(COAST)
+        # Intake control
+        if(R1 == True and R2 == False):
+            print("Forward")
+            intake_motor.spin(REVERSE)
+        elif ((R2 == True) and (R1 == False)):
+            print("Reverse")
+            intake_motor.spin(FORWARD)
         else:
-            brain.screen.clear_screen(Color.BLUE)
-            brain.screen.print("Arcade-Drive Control")
+            intake_motor.stop()
+        
 
-            # Controls for Up-Down and Left-Right movement
-            leftAxis_UpDwn = controller.axis3.position()
-            leftAxis_LR = controller.axis4.position()
+        L1 = bool(controller.buttonL1.pressing)
+        # Catapult Controls
+        if(L1 == True):
+            catapult_motor.spin(FORWARD)
+        else:
+            catapult_motor.stop()
 
-            # Determines whether or not the robot should be going forward or backward, left or right
-            isNegativeY = False if leftAxis_UpDwn >= 0 else True
-            isNegativeX = False if leftAxis_LR >= 0 else True
-
-            # Calculate exponentional based speed
-            ySpeed = (int)(math.exp(abs(leftAxis_UpDwn) / EXPONENTIALCONSTANT))
-            xSpeed = (int)(math.exp(abs(leftAxis_LR) / EXPONENTIALCONSTANT))
-
-            if(ySpeed > 5 or xSpeed > 5):
-                left_motor_group.spin(FORWARD)
-                right_motor_group.spin(FORWARD)
-
-                if(isNegativeY):
-                    ySpeed *= -1
-                if(isNegativeX):
-                    xSpeed *= -1
-
-                # Set the velocity depending on the axis position
-                left_motor_group.set_velocity(ySpeed + xSpeed,PERCENT)
-                right_motor_group.set_velocity(ySpeed - xSpeed,PERCENT)
-            else:
-                left_motor_group.stop(COAST)
-                right_motor_group.stop(COAST)
-
+        # Control Modes: 1 = Manual Tank Drive, 2 = Joystick control, 3 = Joystick control at max speed
+        match Control_Mode :
+            case mode.TANK :
+                tank_drive()
+            case mode.ARCADE :
+                arcade_drive()
+            case mode.ARCADE_SPEED:
+                arcade_speed_drive()
 
         wait(20, MSEC)
 
